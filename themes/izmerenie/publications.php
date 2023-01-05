53225dc0b328fbb200ecbf2b4384ce038b6b0a98
smolanovsergey <smolanovsergey@gmail.com>
2023-01-05 11:17:56 +0200

    главная десктоп

---
+++ themes/izmerenie/publications.php
@@ -0,0 +1,76 @@
+<?php /* Template Name: Публікації */ ?>
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package izmerenie
+ */
+
+get_header();
+$post_id = get_the_ID();
+?>
+<main id="main" class="main">
+    <section class="publications-page">
+        <div class="publications-page__container main-container">
+            <div class="back-button" onclick="window.history.back()">
+                <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle opacity="0.2" cx="21" cy="21" r="21" fill="#BFD9EB"/>
+                    <rect x="23.4854" y="13" width="2" height="12" rx="1" transform="rotate(45 23.4854 13)" fill="#A0BCC8"/>
+                    <rect x="15" y="21.4853" width="2" height="12" rx="1" transform="rotate(-45 15 21.4853)" fill="#A0BCC8"/>
+                </svg>
+            </div>
+            <h1 class="publications-page__title page-title">
+                <?php echo the_field("zagolovok_storinky", $post_id); ?>
+            </h1>
+        </div>
+        <div class="publications-page__list main-container">
+            <?php
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+            $args = array(
+                'posts_per_page' => 9,
+                'order' 	 => 'DESC',
+                'orderby'   => 'menu_order',
+                'post_type' 	 => 'publications',
+                'paged'	         => $paged
+            );
+
+            $MY_QUERY = new WP_Query( $args );
+            if ( $MY_QUERY->have_posts() ) :
+                while ( $MY_QUERY->have_posts() ) : $MY_QUERY->the_post(); ?>
+                    <a href="<?php the_permalink();?>" class="publications-page__item">
+                        <h2 class="publications-page__item-title"><?php echo the_field("zagolovok_zapysu"); ?></h2>
+                        <div class="publications-page__item-excerpt">
+                            <?php echo the_field("korotkyj_opys"); ?>
+                        </div>
+                    </a>
+                <?php endwhile;
+            endif;
+            ?>
+            <?php
+            wp_reset_postdata();
+            ?>
+        </div>
+    </section>
+    <footer id="footer" class="footer footer__static">
+        <div class="footer__container main-container">
+            <div class="footer__bottom">
+                <div class="footer__copyright">
+                    © 2022. Четвертое измерение. Все права защищены.
+                </div>
+                <a class="footer__phone" href="tel:+38(063)000-00-00">+38(063)000-00-00</a>
+                <a class="footer__mail" href="mailto:4izmerenie@4izmerenie.com">4izmerenie@4izmerenie.com</a>
+            </div>
+        </div>
+    </footer>
+</main>
+
+<?php wp_footer(); ?>
+</body>
+</html>
